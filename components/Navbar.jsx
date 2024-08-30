197975fe922eb7a4c58c006e7ebbd6ff4d3640f7
Devkarpenter <deveshkarpenter1998@gmail.com>
2024-08-30 12:29:02 +0530

    Add .env to .gitignore

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import { HiSearch } from "react-icons/hi";
+
+
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center bg-background: #FFFFFF;
+    <nav className="flex justify-between items-center ;
  px-8 py-3">
       <Link className="bg-gray-800 text-white rounded-lg p-2" href={'/addTodo'}>Add Todo</Link>  
         <Link className="font-bold"  href={"/"}>
